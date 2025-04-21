e9513f1dd523dc4323552ea917386a7c0c438d27
Carmen Varela <cvi0004@alu.medac.es>
2025-04-21 18:13:38 +0200

    User finished

--- src/makingSocial/model/User.java
+++ src/makingSocial/model/User.java
@@ -9,5 +9,55 @@ public class User {
         // userType = true , es Host
         private boolean userType = true;
 
-h
+    // constructor
+    public User(String userName, String password, String mail, boolean userType) {
+        this.userName = userName;
+        this.password = password;
+        this.mail = mail;
+        this.userType = userType;
+    }
+
+    //getters & setters
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public boolean isUserType() {
+        return userType;
+    }
+
+    public void setUserType(boolean userType) {
+        this.userType = userType;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", mail='" + mail + '\'' +
+                ", userType=" + userType +
+                '}';
+    }
 }
