90c36f3b6077d69ce0d4ed73ed4a85c2fefbb3e2
Carmen Varela <cvi0004@alu.medac.es>
2025-04-21 14:41:19 +0200

    Last upload 21/04

---
+++ src/makingSocial/model/User.java
@@ -0,0 +1,13 @@
+package makingSocial.model;
+
+public class User {
+    // attribute
+        // nombre usuario
+        private String userName;
+        private String password;
+        private String mail;
+        // userType = true , es Host
+        private boolean userType = true;
+
+
+}
