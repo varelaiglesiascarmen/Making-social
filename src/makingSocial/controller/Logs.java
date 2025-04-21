485a1c5f225b428050f9dc69f7a100ce5f9bf226
Zhou <zh809q3@gmail.com>
2025-04-21 14:30:46 +0200

    Log system

---
+++ src/makingSocial/controller/Logs.java
@@ -0,0 +1,19 @@
+package makingSocial.controller;
+
+public class Logs {
+    // Faltan los módulos necesarios (clases)
+
+
+    // Login_Controller Logs
+
+
+    // SignIn_Controller Logs
+
+
+    // Sesion Out Logs
+
+
+    // System Error Logs (recolecta
+
+
+}
