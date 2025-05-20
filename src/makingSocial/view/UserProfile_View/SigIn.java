009defb89a10b9b9259658bfb04ee33ddb8b308e
Carmen Varela <cvi0004@alu.medac.es>
2025-05-20 12:17:47 +0200

    sigin modified

--- src/makingSocial/view/UserProfile_View/SigIn.java
+++ src/makingSocial/view/UserProfile_View/SigIn.java
@@ -1,9 +1,6 @@
 package makingSocial.view.UserProfile_View;
 
-
-import java.awt.EventQueue;
-
-
+import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -13,16 +10,13 @@ import makingSocial.DAO.SignIn_DAO;
 import makingSocial.model.UserModel;
 
 import javax.swing.JLabel;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 
-
 public class SigIn extends JFrame {
-
 
     private static final long serialVersionUID = 1L;
     private JPanel contentPane;
@@ -31,12 +25,8 @@ public class SigIn extends JFrame {
     private JPasswordField passwordField;
     private JPasswordField passwordFieldConfirm;
     private JTextField textField;
-    private JTextField textField_Edad;
+    private JTextField textFieldAge;
 
-
-    /**
-     * Launch the application.
-     */
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -51,20 +41,14 @@ public class SigIn extends JFrame {
         });
     }
 
-
-    /**
-     * Create the frame.
-     */
     public SigIn() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 960, 700);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-
         setContentPane(contentPane);
         contentPane.setLayout(null);
-
 
         JLabel SignInTitle = new JLabel("Registrate");
         SignInTitle.setHorizontalAlignment(SwingConstants.CENTER);
@@ -72,13 +56,11 @@ public class SigIn extends JFrame {
         SignInTitle.setBounds(246, 80, 415, 74);
         contentPane.add(SignInTitle);
 
-
         JLabel Name = new JLabel("Nombre");
         Name.setHorizontalAlignment(SwingConstants.RIGHT);
         Name.setFont(new Font("Tahoma", Font.PLAIN, 20));
         Name.setBounds(246, 196, 181, 37);
         contentPane.add(Name);
-
 
         JLabel Password = new JLabel("Contraseña");
         Password.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -86,13 +68,11 @@ public class SigIn extends JFrame {
         Password.setBounds(246, 243, 181, 37);
         contentPane.add(Password);
 
-
         JLabel PassWordConfirm = new JLabel("Confirmar contraseña");
         PassWordConfirm.setHorizontalAlignment(SwingConstants.RIGHT);
         PassWordConfirm.setFont(new Font("Tahoma", Font.PLAIN, 20));
         PassWordConfirm.setBounds(192, 290, 235, 37);
         contentPane.add(PassWordConfirm);
-
 
         JLabel E_Mail = new JLabel("e-mail");
         E_Mail.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -100,35 +80,29 @@ public class SigIn extends JFrame {
         E_Mail.setBounds(246, 337, 181, 37);
         contentPane.add(E_Mail);
 
-
         JLabel Age = new JLabel("Edad");
         Age.setHorizontalAlignment(SwingConstants.RIGHT);
         Age.setFont(new Font("Tahoma", Font.PLAIN, 20));
         Age.setBounds(246, 384, 181, 37);
         contentPane.add(Age);
 
-
         textFieldName = new JTextField();
         textFieldName.setBounds(437, 196, 181, 37);
         contentPane.add(textFieldName);
         textFieldName.setColumns(10);
-
 
         textFieldEMAil = new JTextField();
         textFieldEMAil.setColumns(10);
         textFieldEMAil.setBounds(437, 337, 181, 37);
         contentPane.add(textFieldEMAil);
 
-
         passwordField = new JPasswordField();
         passwordField.setBounds(437, 243, 181, 37);
         contentPane.add(passwordField);
 
-
         passwordFieldConfirm = new JPasswordField();
         passwordFieldConfirm.setBounds(437, 290, 181, 37);
         contentPane.add(passwordFieldConfirm);
-
 
         JButton btnOK = new JButton("Confirmar");
         btnOK.setBounds(387, 461, 140, 29);
@@ -145,16 +119,16 @@ public class SigIn extends JFrame {
         textField.setBounds(437, 149, 181, 37);
         contentPane.add(textField);
 
-        textField_Edad = new JTextField();
-        textField_Edad.setColumns(10);
-        textField_Edad.setBounds(437, 384, 181, 37);
-        contentPane.add(textField_Edad);
+        textFieldAge = new JTextField();
+        textFieldAge.setColumns(10);
+        textFieldAge.setBounds(437, 384, 181, 37);
+        contentPane.add(textFieldAge);
 
         btnOK.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Cuidado: los JPasswordField devuelven char[], hay que convertirlos
-                String nickname = NickName.getText();
+                String nickname = textField.getText(); // Corregido: textField contiene el nombre de usuario
                 String password = new String(passwordField.getPassword());
                 String passwordConfirm = new String(passwordFieldConfirm.getPassword());
                 String email = textFieldEMAil.getText();
