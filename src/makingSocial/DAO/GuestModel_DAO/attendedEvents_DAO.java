7265a6b8f60199ef306c0bb377875044c4749716
Carmen Varela <cvi0004@alu.medac.es>
2025-05-22 17:51:41 +0200

    SearchEventPublic_view & attendEvents_DAO code modified

--- src/makingSocial/DAO/GuestModel_DAO/attendedEvents_DAO.java
+++ src/makingSocial/DAO/GuestModel_DAO/attendedEvents_DAO.java
@@ -1,4 +1,7 @@
 package makingSocial.DAO.GuestModel_DAO;
 
 public class attendedEvents_DAO {
+    public class obtenerEventosAsistidosPorUsuario(int ID_Event){
+
+    }
 }
--- src/makingSocial/view/GuestModel_View/attendedEvents.java
+++ src/makingSocial/view/GuestModel_View/attendedEvents.java
@@ -1,10 +1,16 @@
 package makingSocial.view.GuestModel_View;
 
+import makingSocial.model.EventModel;
+import makingSocial.view.UserProfile_View.HomePage;
+
 import java.awt.EventQueue;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
 public class attendedEvents extends JFrame {
 
@@ -48,21 +54,56 @@ public class attendedEvents extends JFrame {
         titleAttendEvent.setBounds(58, 51, 640, 90);
         contentPane.add(titleAttendEvent);
 
-        // Listados de los eventos a los que ha asistido
-        JLabel AttendEvent = new JLabel("Lorem ipsum dolor sit amet");
-        AttendEvent.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        AttendEvent.setBounds(58, 176, 583, 33);
-        contentPane.add(AttendEvent);
+        // DAO: obtener eventos asistidos por el usuario
+        List <EventModel> eventosAsistidos = new AttendedEvents_DAO().obtenerEventosAsistidosPorUsuario(EventModel.getID_Event());
 
-        // botón de acceder
-        JButton btnNewButton = new JButton(".");
-        btnNewButton.setBounds(651, 178, 42, 36);
-        contentPane.add(btnNewButton);
+        if (eventosAsistidos.isEmpty()) {
+            JLabel sinEventos = new JLabel("¡Aún no has asistido a ningún evento! Busca tu próximo evento en la página de inicio.");
+            sinEventos.setFont(new Font("Tahoma", Font.PLAIN, 20));
+            sinEventos.setBounds(58, 176, 800, 33);
+            contentPane.add(sinEventos);
+        } else {
+            int y = 176;
+            for (EventModel evento : eventosAsistidos) {
+                // imprime el codigo identificativo
+                JLabel lblCode = new JLabel(String.valueOf(evento.getID_Event()));
+                lblCode.setFont(new Font("Tahoma", Font.PLAIN, 20));
+                lblCode.setBounds(58, y, 583, 33);
+                contentPane.add(lblCode);
+
+                JButton btnExplorar = new JButton("Explorar");
+                btnExplorar.setBounds(660, y, 100, 33);
+                contentPane.add(btnExplorar);
+
+                btnExplorar.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        socialSearch socialchat = new socialSearch();
+                        socialchat.setVisible(true);
+                        dispose();
+                    }
+                });
+
+                y += 50;
+            }
+        }
 
         JButton btnGoHomePage = new JButton("Volver a inicio");
         btnGoHomePage.setFont(new Font("Tahoma", Font.PLAIN, 15));
         btnGoHomePage.setBounds(784, 606, 140, 30);
         contentPane.add(btnGoHomePage);
+
+        btnGoHomePage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // llamar a la ventana Homepage
+                HomePage homepage = new HomePage();
+                homepage.setVisible(true);
+
+                // disppuse() cierra la venta
+                dispose();
+            }
+        });
 
     }
 }
